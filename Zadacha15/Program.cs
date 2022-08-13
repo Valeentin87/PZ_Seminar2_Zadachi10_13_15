21d85ff0a7b667839bb57a08b02255bebabc8d7d
Valeentin87 <Valeentin87@list.ru>
2022-08-13 15:13:14 +0300

    Сохранение перед отладкой

--- Zadacha15/Program.cs
+++ Zadacha15/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Напишите программу, которая на вход принимает цифру, обозначающую день недели и проверяет не является ли этот день выходным
-
+string Vihodnoy ()
+{
+    Console.WriteLine("Введите цифру, обозначающую порядковый день в неделе");
+    int day = Convert.ToInt32(Console.Readline());
+    if (day == 6 || day == 7)
+    {
+        string otvet = "день является выходным";
+        Console.WriteLine(otvet);
+        return otvet;
+    }
+    else
+    {
+        string otvet = "день является рабочим";
+        Console.WriteLine(otvet);
+        return otvet;
+    }
+    
+}
+Vihodnoy ();
