44c2ac81258a969e9c1809672d77abad67e0d78f
Valeentin87 <Valeentin87@list.ru>
2022-08-13 17:14:13 +0300

    Доработал с учётом замечаний

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -1,11 +1,9 @@
 ﻿/* Задача 10. Напишите программу, которая принимает на вход трёхзначное чсило и на выходе показывает вторую цифру этого числа*/
+Console.WriteLine("Введите трёх значное число");
+char Vtoroe = SecondNumber(Console.ReadLine());
+Console.WriteLine($"Второе число равно = {Vtoroe}");
 char SecondNumber(string number)
 {
 char NumberTwo = Convert.ToChar(number[1]);
-Console.WriteLine($"Было введено трёхзначное число {number}");
-Console.WriteLine($"Значение второй цифры равно = {NumberTwo}");
 return NumberTwo;
 }
-SecondNumber("456");
-SecondNumber("782");
-SecondNumber("918");
