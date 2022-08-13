40e33fe6319a0b0a5d78fe6baa617cc475151ea9
Valeentin87 <Valeentin87@list.ru>
2022-08-13 14:49:55 +0300

    Успешное решение задачи 13

--- Zadacha13/Program.cs
+++ Zadacha13/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Задача 13: напишите программу которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
-
+string numberThRee ()
+{
+    Console.WriteLine("Введите целое число, не менее трёх знаков");
+    string Nstring = Console.ReadLine();
+    //char Nchar = Convert.ToChar(Nstring[2]);
+    if (Nstring.Length < 3) 
+    {
+        string name = "Во введенном числе третьей цифры нет";
+        Console.WriteLine("Во введенном числе третьей цифры нет");
+        return name;
+    }
+    else
+    {
+        //int Nint = Convert.ToInt32(Nchar);
+        Console.WriteLine($"Третья цифра во введенном числе равна {Nstring[2]}");
+        return Convert.ToString(Nstring[2]);
+    }
+} 
+numberThRee();
