e291f36125429704a604e6f3de0dd6d9395d3136
Valeentin87 <Valeentin87@list.ru>
2022-08-13 13:47:45 +0300

    добавил гитигнор

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10. Напишите программу, которая принимает на вход трёхзначное чсило и на выходе показывает вторую цифру этого числа*/
+char SecondNumber(string number)
+{
+char NumberTwo = Convert.ToChar(number[1]);
+Console.WriteLine($"Было введено трёхзначное число {number}");
+Console.WriteLine($"Значение второй цифры равно = {NumberTwo}");
+return NumberTwo;
+}
+SecondNumber("456");
+SecondNumber("782");
+SecondNumber("918");
